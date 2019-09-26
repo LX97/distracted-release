a2215040139bc451b3f96472d3ba4fdf1c97fbd8
Brandon Haworth <m.brandon.haworth@gmail.com>
2019-09-25 22:17:48 -0400

    Add simple destroy script

---
+++ Assets/Scripts/ReachAndDestroy.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReachAndDestroy : MonoBehaviour {
+	/// <summary>
+	/// The reached destination distance.
+	/// </summary>
+	public float reachedDestinationDistanceSquared;
+
+
+	/// <summary>
+	/// The agent.
+	/// </summary>
+	private UnityEngine.AI.NavMeshAgent agent;   
+
+
+	/// <summary>
+	/// Start this instance.
+	/// </summary>
+	void Start () {
+		agent = GetComponentInChildren<UnityEngine.AI.NavMeshAgent>();
+	}
+
+
+	/// <summary>
+	/// Update this instance.
+	/// </summary>
+	void Update () {
+		if(Vector3.SqrMagnitude(transform.position - agent.destination) < reachedDestinationDistanceSquared)
+			GameObject.Destroy(transform.gameObject);
+	}
+}
